99bbaef04d6c9c4176710d844393ab840918d9eb
hariharan <harikalam143@gmail.com>
2024-06-14 10:46:31 +0000

    server port number issue

---
+++ src/components/MoneyManager/index.js
@@ -0,0 +1,24 @@
+import {Component} from 'react'
+const transactionTypeOptions = [
+  {
+    optionId: 'INCOME',
+    displayText: 'Income',
+  },
+  {
+    optionId: 'EXPENSES',
+    displayText: 'Expenses',
+  },
+]
+
+// Write your code here
+class MoneyManager extends Component {
+  render() {
+    return (
+      <div>
+        <h1>hello...</h1>
+      </div>
+    )
+  }
+}
+
+export default MoneyManager
